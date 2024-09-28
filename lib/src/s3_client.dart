4a3e188bb7a8d706259d408d06eb75324cd5ed7b
Wes Johnson <wes@solwey.com>
2024-09-28 14:56:37 -0700

    ...

--- lib/src/s3_client.dart
+++ lib/src/s3_client.dart
@@ -244,7 +244,9 @@ class StorageClient {
     void Function(int)? onProgress,
   ) {
     final url = getRequestUrl(bucket, object, resource, queries);
+    print('url: $url');
     final request = StorageRequest(method, url, onProgress: onProgress);
+    print('request: $request');
     request.headers['host'] = url.authority;
 
     if (headers != null) {
@@ -283,6 +285,8 @@ class StorageClient {
       if (query.isNotEmpty) query.write('&');
       query.write(encodeQueries(queries));
     }
+
+    print('path: $path');
 
     return Uri(
       scheme: s3storage.useSSL ? 'https' : 'http',
